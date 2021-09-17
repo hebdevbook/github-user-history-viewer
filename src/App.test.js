179c1f5a42cd6f18cea2a87765e6eedec2fd321f
Ran Bar-Zik <ran@bar-zik.com>
2021-09-17 12:03:37 +0300

    add publish

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders container', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const containerElement = screen.getByTestId('general-repo-container');
+  expect(containerElement).toBeInTheDocument();
 });
